78503a970a5e1a6465a21cb232d8cb2a8b6c8cde
Lutkowski <anton.lutkovskiy@gmail.com>
2024-06-13 02:39:07 +0300

    refactor: facilities icons

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -43,22 +43,22 @@ const Map = () => {
     const customIcons: { [key in FacilityType]: Icon } = {
         [FacilityType.Elevator]: new Icon({
             iconUrl: '/elevator.svg',
-            iconSize: [24, 24],
+            iconSize: [30, 30],
             iconAnchor: [12, 12],
         }),
         [FacilityType.EscalatorUp]: new Icon({
             iconUrl: '/escalator_up.svg',
-            iconSize: [24, 24],
+            iconSize: [30, 30],
             iconAnchor: [12, 12],
         }),
         [FacilityType.EscalatorDown]: new Icon({
             iconUrl: '/escalator_down.svg',
-            iconSize: [24, 24],
+            iconSize: [30, 30],
             iconAnchor: [12, 12],
         }),
         [FacilityType.Entrance]: new Icon({
             iconUrl: '/entrance.svg',
-            iconSize: [24, 24],
+            iconSize: [30, 30],
             iconAnchor: [12, 12],
         }),
     };
