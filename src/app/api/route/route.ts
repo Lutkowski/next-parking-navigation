3b54d8b9209990ef63ca4d443de1ac8157d3c3a3
Lutkowski <anton.lutkovskiy@gmail.com>
2024-06-19 16:02:54 +0300

    refactor: make route endpoint for all floor

--- src/app/api/route/route.ts
+++ src/app/api/route/route.ts
@@ -1,20 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { dijkstraUndirected, getPath } from '@/lib/utils/dijkstra';
-import { graph3, graphData3 } from '@/lib/constants/graphs/graph3';
+import { graphs } from '@/lib/constants/graphs';
 import dbConnect from "@/lib/utils/dbConnect";
 import Shop from "@/models/Shop";
 
-const findShopIdByName = async (name: string): Promise<string | null> => {
+const findShopByName = async (name: string) => {
     await dbConnect();
     const shop = await Shop.findOne({ slug: name });
-    if (shop) {
-        return shop._id;
-    }
-    return null;
+    return shop;
 };
 
-const findNodeByShopId = (shopId: string): string | null => {
-    const node = graphData3.nodes.find((n: any) => n.id === shopId);
+const findNodeByShopId = (shopId: string, graphData: any): string | null => {
+    const node = graphData.nodes.find((n: any) => n.id === shopId);
     return node ? node.id : null;
 };
 
@@ -28,31 +25,39 @@ export async function GET(req: NextRequest) {
         return NextResponse.json({ error: 'Both startShopName and endShopName are required' }, { status: 400 });
     }
 
-    const startShopId = await findShopIdByName(startShopName);
-    const endShopId = await findShopIdByName(endShopName);
+    const startShop = await findShopByName(startShopName);
+    const endShop = await findShopByName(endShopName);
 
-    if (!startShopId || !endShopId) {
+    if (!startShop || !endShop) {
         return NextResponse.json({ error: 'Shop not found' }, { status: 404 });
     }
 
-    const startNodeId = findNodeByShopId(startShopId);
-    const endNodeId = findNodeByShopId(endShopId);
+    const startFloor = startShop.floor;
+    const endFloor = endShop.floor;
+
+    if (startFloor !== endFloor) {
+        return NextResponse.json({ error: 'Shops are on different floors' }, { status: 400 });
+    }
+
+    // @ts-ignore
+    const { graph, graphData } = graphs[startFloor];
+
+    const startNodeId = findNodeByShopId(startShop._id, graphData);
+    const endNodeId = findNodeByShopId(endShop._id, graphData);
 
     if (!startNodeId || !endNodeId) {
         return NextResponse.json({ error: 'Graph node not found for the given shop' }, { status: 404 });
     }
 
-    const graph = graph3;
-
     const { previous } = dijkstraUndirected(graph, startNodeId);
     const path = getPath(previous, startNodeId, endNodeId);
     const coordinates = path.map(node => {
-        const graphNode = graphData3.nodes.find((n: any) => n.id === node);
+        const graphNode = graphData.nodes.find((n: any) => n.id === node);
         if (!graphNode) {
             throw new Error(`Node with id ${node} not found in graph data`);
         }
         return graphNode.coordinates as [number, number];
     });
 
-    return NextResponse.json({floor:3, coordinates :coordinates}, { status: 200 });
+    return NextResponse.json({ floor: startFloor, coordinates }, { status: 200 });
 }
---
+++ src/lib/constants/graphs/index.ts
@@ -0,0 +1,11 @@
+import {graph1, graphData1} from './graph1';
+import {graph2, graphData2} from './graph2';
+import {graph3, graphData3} from './graph3';
+
+export const graphs = {
+    1: {graph: graph1, graphData: graphData1},
+    2: {graph: graph2, graphData: graphData2},
+    3: {graph: graph3, graphData: graphData3},
+    '-1': null,
+    '-2': null,
+};
