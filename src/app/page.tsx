b387858e9b9c7947831fa3edc62792cc929429d8
Lutkowski <anton.lutkovskiy@gmail.com>
2024-06-26 08:29:32 +0300

    feat: add favicon and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import dynamic from 'next/dynamic'
 import {FloorProvider} from "@/contexts/FloorContext";
+import {Metadata} from "next";
 
 const DynamicMap = dynamic(() => import('../components/map/Map'), {
     ssr: false
 });
+
+export const metadata: Metadata = {
+    title: 'Parking App',
+    icons:{
+        icon:'/favicon.svg'
+    }
+}
 
 export default function Home() {
     return (
